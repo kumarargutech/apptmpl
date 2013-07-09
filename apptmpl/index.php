024d4628e5a44935fb30ef3fe1a2dafca2e49fd3
weblogicx <kumar@weblogicxindia.com>
2013-07-09 12:42:09 +0530

    - base files

---
+++ apptmpl/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  Templates.apptemplate
+ *
+ * @copyright   Copyright (C) 2013 - 2021 Weblogicx India Private Limited. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
